b5cee92a114c386e482d266292556addb1f6eb94
wonjongin <wonggamggik@gmail.com>
2024-03-04 11:53:50 +0900

    step1-body-html

--- src/components/ObjectOne/ObjectImage/ObjectImage.jsx
+++ src/components/ObjectOne/ObjectImage/ObjectImage.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function ObjectImage() {
-  return <div>p</div>;
+  return (
+    <div>
+      <img
+        className="picture__1"
+        src="/images/flyclapping/flyclapping_orange.png"
+        alt="flyclapping_orange"
+      ></img>
+      <img
+        className="picture__2"
+        src="/images/flyclapping/flyclapping_white.png"
+        alt="flyclapping_white"
+      ></img>
+      <img
+        className="picture__3"
+        src="/images/flyclapping/flyclapping_yellow.png"
+        alt="flyclapping_yellow"
+      ></img>
+    </div>
+  );
 }
