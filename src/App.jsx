3bb2c7a8b14c7f9f7972e7db8d1f0f474a492be6
wonjongin <wonggamggik@gmail.com>
2024-03-05 14:15:03 +0900

    step4-header-divider

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./components/TitleHeader/TitleHeader";
 import ItemsDisplay from "./components/ObjectsListBody/ObjectsListBody";
+import Divider from "./components/Divider/Divider";
 
 function App() {
   return (
     <div>
       <Header />
+      <Divider />
       <ItemsDisplay />
     </div>
   );
---
+++ src/components/Divider/Divider.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Divider.module.css";
+
+export default function Divider() {
+  return (
+    <div className={styles.wave}>
+      <img src="/images/divider/waves.svg"></img>
+    </div>
+  );
+}
